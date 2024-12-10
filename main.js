c5010f92eb393fb6e24535fc1f0b18b42e34f085
Hugo-Andres17 <hugoapm17@gmail.com>
2024-12-10 17:31:26 -0300

    version 1.0.2

--- main.js
+++ main.js
@@ -6,23 +6,30 @@ function loguear (){
 
     do{
 
-        let usuario = prompt ("ingresa tu usuario")
+        let usuario = prompt ("ingresa tu usuario").toUpperCase()
         
 
         if (usuario == null || usuario == ""){
             alert ("no escribiste nada, no se puede reconocer el usuario")
-            break
+            intentos++
+            if(intentos>3){
+                alert("superaste la cantidad de intentos")
+                break
+            }
+            
+        
         }
 
-        if((usuario == "admin"|| usuario=="hugo") && intentos<=3){
-            let contraseña = prompt ("ingresa tu contraseña")
+        if((usuario === "ADMIN"|| usuario==="HUGO") && intentos<=3){
+            let contraseña = parseInt (prompt("ingresa tu contraseña"))
             if (contraseña == null){
                 alert ("no ingresaste la contraseña asi que me toca sacarte otra vez")
                 break;
             }
-            if (contraseña === "12345678"){
+            if (contraseña === 12345678){
                 alert("Bienvenido " + usuario)
                 identificar = false
+                sumar()
             }else{
                 alert ("contraseña incorrecta")
                 intentos++ 
@@ -42,3 +49,17 @@ function loguear (){
 }
 
 loguear()
+
+function sumar() {
+
+    alert("estas usando la function de sumar, preciona enter para comenzar a sumar ")
+
+let num1 = parseFloat(prompt("ingresa un numero"))
+let num2 = parseFloat(prompt("ingresa el segundo numero "))
+
+let resultado = num1 + num2 ;
+
+alert ("el resultado es " + resultado)
+
+}
+
